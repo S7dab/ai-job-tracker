308f3f50eb90a3419e1fc17c68316d1efc4e2493
Shadab Ahmad <s7dab1998@gmail.com>
2026-09-15 19:46:07 +0530

    Update CORS for frontend production frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://ai-job-tracker-shadab.netlify.app",
   }),
 );
 app.use(helmet());

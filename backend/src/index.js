2129de4bf5afde6caaaa94a2257c1e2c7530e7a6
Shadab Ahmad <s7dab1998@gmail.com>
2026-09-15 20:00:09 +0530

    fix cros production

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://ai-job-tracker-shadab.netlify.app",
+    origin:["https://ai-job-tracker-shadab.netlify.app","http://localhost:5173/"],
   }),
 );
 app.use(helmet());
